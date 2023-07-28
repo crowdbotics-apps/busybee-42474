94010be266a6382a99fcbfaf78aed561d5dd644c
Crowdbotics <team@crowdbotics.com>
2023-07-28 01:12:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "busybee-42474",
+  url: "https://busybee-42474.botics.co",
+  api: "https://busybee-42474.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
